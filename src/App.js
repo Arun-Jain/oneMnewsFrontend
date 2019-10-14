33e573cb3417035161bb4a27a7757c9ca7c919f2
aman jain <amanj9641@gmail.com>
2019-10-14 23:07:06 +0530

    patching all news

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 
 class App extends React.Component {
 
+	state = {
+		hadErrors: false,
+		news: {}
+	};
+
+	componentDidMount() {
+		fetch("http://127.0.0.1:8000/news/")
+		.then(res => res.json())
+		.then(res => this.setState({ news: res }))
+		.catch(() => this.setState({ hadErrors: true }));
+	}
+
 	render() {
 		return (
-			<h1>Hello World!</h1>
+			<div>{JSON.stringify(this.state.news)}</div>
 		)
 	}
 }
